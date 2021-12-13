4f9d96ea42b6412e4ab0b9a31af65cd5e4a98046
Adivair_Ramos <a193325@dac.unicamp.br>
2021-12-13 09:44:00 -0300

    MVC pattern

--- routes/admin.js
+++ routes/admin.js
@@ -2,28 +2,12 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+router.get("/add-product", productsController.getAddProduct);
 
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    productsCSS: true,
-    formsCSS: true,
-    activeAddProducts: true
-  });
-});
+router.post("/add-product", productsController.postAddProduct);
 
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  console.log(req.body);
-  res.redirect("/");
-});
-
-exports.routes = router;
-
-exports.products = products;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,22 +2,10 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const adminData = require("./admin");
-
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "My Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productsCSS: true
-  });
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
